f56b6d12c20764e9a3fcec628014817c69fffcb3
Andrew He <andrew@mopub.com>
2011-06-15 11:21:41 -0700

    iOS: initial test project commit

---
+++ MoPubiOS/MoPubTests/MPAdViewTests.m
@@ -0,0 +1,36 @@
+#import <GHUnitIOS/GHUnit.h>
+#import <OCMock/OCMock.h>
+
+#import "MPAdView.h"
+#import <objc/runtime.h>
+
+@interface MPAdViewTests : GHTestCase {
+	MPAdView *adView;
+	id mockView;
+	id mockDelegate;
+}
+@end
+
+@implementation MPAdViewTests
+
+// Run before each test method
+- (void)setUp { }
+
+// Run after each test method
+- (void)tearDown { }
+
+// Run before the tests are run for this class
+- (void)setUpClass {
+	adView = [[MPAdView alloc] init];
+	mockView = [OCMockObject partialMockForObject:adView];
+	mockDelegate = [OCMockObject mockForProtocol:@protocol(MPAdViewDelegate)];
+}
+
+- (void)tearDownClass {
+}
+
+- (void)testSimplePass {
+	GHAssertFalse(NO, @"this should be false");
+}
+
+@end
